20f6ac7883db79217501d59b1bc7abd7a7f340ab
wenbin.zhang <wenbin.zhang@Ctrip.com>
2020-05-27 13:30:37 +0800

    fix: delete output

---
+++ libs/index.ts
@@ -0,0 +1,14 @@
+import Axios from "./axios";
+
+import Log from "./log";
+
+import Trace from "./trace";
+
+import Tools from "./tools";
+
+export {
+  Axios,
+  Log,
+  Trace,
+  Tools
+}
---
+++ tests/tools.spec.js
@@ -0,0 +1,6 @@
+import { Tools } from "../index";
+// import * as reactLibs from "../index"
+
+test("Tools test", () => {
+  expect(Tools.environment()).toBe(0);
+})
